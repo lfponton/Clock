a972293cb6b2e18e1cce4df367bab11579704e6c
Luis <lfponton@gmail.com>
2021-03-31 11:14:13 +0200

    Implemented one parameter set() and getTimeInSeconds tests

--- test/ClockTest.java
+++ test/ClockTest.java
@@ -22,6 +22,7 @@ class ClockTest
     return clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond();
   }
 
+  // Test methods for three parameters set()
   @Test void setZero()
   {
     clock.set(0,0,0);
@@ -79,5 +80,88 @@ class ClockTest
     assertThrows(IllegalArgumentException.class, () -> clock.set(0,0,71));
     assertThrows(IllegalArgumentException.class, () -> clock.set(0,71,0));
     assertThrows(IllegalArgumentException.class, () -> clock.set(41,0,0));
+  }
+
+  // Test methods for one parameter set()
+
+  @Test void set1ArgsZero()
+  {
+    clock.set(0);
+    assertEquals("0:0:0", valueOf(clock));
+  }
+
+  @Test void set1ArgsOne()
+  {
+    clock.set(0);
+    assertEquals("0:0:1", valueOf(clock));
+  }
+
+  @Test void set1ArgsMany()
+  {
+    clock.set(185);
+    assertEquals("0:3:5", valueOf(clock));
+    clock.set(86000);
+    assertEquals("23:53:20", valueOf(clock));
+    clock.set(86000 + 86400); // tomorrow
+    assertEquals("23:53:20", valueOf(clock));
+    clock.set(86000 + 7 * 86400); // one week later
+    assertEquals("23:53:20", valueOf(clock));
+  }
+
+  @Test void set1ArgsBoundary()
+  {
+    // right lower bound: 0 [already done in set1ArgsZero()]
+
+    // left lower bound: -1
+    assertThrows(IllegalArgumentException.class, () -> clock.set(-1));
+
+    // left upper bound: 86399
+    clock.set(86399);
+    assertEquals("23:59:59", valueOf(clock));
+
+    // right upper bound: 86400
+    clock.set(86400);
+    assertEquals("0:0:0", valueOf(clock));
+  }
+
+  @Test void set1ArgsException()
+  {
+    assertThrows(IllegalArgumentException.class, () -> clock.set(-26));
+    assertThrows(IllegalArgumentException.class, () -> clock.set(-90000));
+  }
+
+  @Test void getTimeInSecondsZero()
+  {
+    assertEquals(0, clock.getTimeInSeconds());
+  }
+
+  @Test void getTimeInSecondsOne()
+  {
+    clock.set(0,0,1);
+    assertEquals(1,clock.getTimeInSeconds());
+  }
+
+  @Test void getTimeInSecondsMany()
+  {
+    clock.set(0, 3, 5);
+    assertEquals(185, clock.getTimeInSeconds());
+    clock.set(23, 53, 20);
+    assertEquals(86000, clock.getTimeInSeconds());
+  }
+
+  @Test void getTimeInSecondsBoundary()
+  {
+    // lower left: -1 [not possible]
+    // lower right: 0 [already done in getTimeInSecondsZero()]
+    // upper right: 0 [already done in getTimeInSecondsZero()]
+
+    // upper left
+    clock.set(23, 59, 59);
+    assertEquals(86399, clock.getTimeInSeconds());
+  }
+
+  @Test void getTimeInSecondsException()
+  {
+    // No exceptions can be thrown
   }
 }
